1de2aa54e45aaa2692e9a615444a205f34a516e4
BrianKemboi <22851850+BrianKemboi@users.noreply.github.com>
2021-04-16 16:06:01 +0300

    todo model

---
+++ src/models/todo.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: false,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  text: string;
+
+  @property({
+    type: 'boolean',
+  })
+  done?: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
